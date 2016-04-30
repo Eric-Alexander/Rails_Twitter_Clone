69eb39c7663438c7c9438fd6d07e431f43f54f9c
EricAlexanderG <EricAlexanderG@gmail.com>
2016-04-30 12:54:16 -0700

    Logged_in? back for heroku

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,10 @@
 class StaticPagesController < ApplicationController
     def home
-        # if logged_in?
-        # @post = current_user.posts.build if logged_in?
-        # @feed_items = current_user.feed.paginate(page: params[:page])
-    # end
+        if logged_in?
+        @post = current_user.posts.build if logged_in?
+        @feed_items = current_user.feed.paginate(page: params[:page])
+    else
+    end
     end
     def about
     end
