a991c9d19db6b97ee332184130c9737ae80815a5
EricAlexanderG <EricAlexanderG@gmail.com>
2016-07-07 13:14:14 -0700

    following associations complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
     has_many :posts, dependent: :destroy
+    has_many :active_relationships, class_name: "Relationship",
+                                    foreign_key: "follower_id",
+                                    dependent: :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :passive_relationships, class_name: "Relationship",
+                                foreign_key: "followed_id",
+                                dependent: :destroy
+    has_many :followers, through: :passive_relationships, source: :follower
     before_save { self.email = email.downcase }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
@@ -10,5 +18,15 @@ class User < ActiveRecord::Base
                         uniqueness: { case_sensitive: false }
     def feed
         Post.where("user_id = ?", id)
+    end
+
+    def follow(other_user)
+      active_relationships.create(followed_id: other_user.id)
+    end
+    def unfollow(other_user)
+      active_relationships.find_by(followed_id: other_user.id).destroy
+    end
+    def following?(other_user)
+      following.include?(other_user)
     end
 end
